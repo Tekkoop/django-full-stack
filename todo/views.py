2d933eb9cbe743b6fbcbd265152b981a35dc4dbf
Tekkoop Tekkoop <tekkoop@tutanota.com>
2023-04-19 10:49:53 +0000

    Added url path to render hello

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, HttpRespone
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def say_hello(request):
-    return HttpRespone("Hello!")
+    return HttpResponse("Hello!")
